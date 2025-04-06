40f832df34cedb704a2564e2f4811ad22013cedf
OmerTeymurov <bussinesb491@gmail.com>
2025-04-06 20:26:39 +0400

    Function

---
+++ Js-Task-Functions/script.js
@@ -0,0 +1,92 @@
+//Task1
+/*function logGreeting(name) {
+    console.log(`${name} is a JS developer`);
+  }
+  
+  logGreeting("Omar");
+ */ 
+
+//Task2
+/*function Userstatus(username) {
+    console.log(`${username} is active now`);
+  }
+  
+Userstatus("Omar");
+Userstatus("Murad");
+Userstatus("Ibrahim");
+ */
+
+//Task3 
+/*function findMax(a, b, c) {
+    const max = Math.max(a, b, c); 
+    console.log(`The Big number is: ${max}`);
+  }
+  
+  findMax(10, 20, 30); // 30
+  findMax(5, 15, 10);   // 15
+  findMax(100, 50, 75); // 100
+  */
+
+//Task5
+
+/* function findMin(a, b, c) {
+    const min = Math.min(a, b, c); 
+    console.log(`The Small number is: ${min}`);
+  }
+
+  findMin(10, 20, 30); 
+  findMin(5, 15, 10);  
+  findMin(100, 50, 75); 
+*/
+
+//Task6
+
+/*function concatenateStrings(str1, str2) {
+  return str1 + str2;
+}
+
+console.log(concatenateStrings("Omar", "Teymurov")); 
+*/
+
+//Task7
+
+/*function calculateRectangleArea(length, width) {
+  return length * width;
+}
+
+console.log(calculateRectangleArea(9, 9)); 
+*/
+
+
+//Task8 
+
+/*function isEven(number) {
+  if (number % 2 === 0) {
+    console.log(number + " cütdür.");
+  } else {
+    console.log(number + " təkdir.");
+  }
+}
+
+console.log(isEven())
+*/
+
+//Task9
+
+/*function sade_eded_olub_olmadigi(number) {
+  if (number < 2) {
+    return console.log(number + " sade deyil");
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return console.log(number + " sade deyil");
+    }
+  }
+
+  return console.log(number + " sadedir");
+}
+
+sade_eded_olub_olmadigi(11); // 11 sadedir
+sade_eded_olub_olmadigi(10); // 10 sade deyil
+*/
